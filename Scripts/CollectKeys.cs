cb61a630d12e05a9bdfd5659f55ae3c08ce7087a
Mittal9269 <72140620+Mittal9269@users.noreply.github.com>
2021-02-28 15:05:31 +0530

    Add files via upload

---
+++ Scripts/CollectKeys.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectKeys : MonoBehaviour
+{
+
+   public GameObject CountBox;
+   public AudioSource collectSound;
+
+   void OnTriggerEnter(){
+       KeysCount.currentCount += 1; 
+       collectSound.Play();
+       Destroy(gameObject);
+   }
+}
